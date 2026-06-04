f975034bbc2076d7d14a4febd6d68196517755b3
Cifer <cifertech@gmail.com>
2026-06-05 02:22:47 +0330

    .

--- apps/web/scripts/copy-404.mjs
+++
@@ -1,7 +0,0 @@
-import { copyFileSync } from "node:fs";
-import { join } from "node:path";
-
-const indexPath = join("dist", "index.html");
-const fallbackPath = join("dist", "404.html");
-
-copyFileSync(indexPath, fallbackPath);
